718579e3e07eccb9aff850d896542e388cb40117
Alexandre Böhs <151465216+XandBohs@users.noreply.github.com>
2025-12-09 11:35:21 +0000

    voltando uma versão

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 function Home() {
     return <div>
-        <h1>Eu te amo muito minha princesa!</h1>
-        Estudei um pouco enquanto você estava fazendo a prova.<br></br>
-        Espero que tenha se saído bem. 💖
+        Mudança ao vivo para meu amor
     </div>
 }
 
