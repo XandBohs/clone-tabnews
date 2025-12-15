abc38a175aa7c79fd9a6fc43d44cc36668f84882
Alexandre Böhs <151465216+XandBohs@users.noreply.github.com>
2025-12-15 01:34:10 +0000

    adiciona os scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return <div>
-        <h1>Mudança ao vivo para meu amor</h1>
+  return (
+    <div>
+      <h1>Mudança ao vivo para meu amor</h1>
     </div>
+  );
 }
 
 export default Home;
