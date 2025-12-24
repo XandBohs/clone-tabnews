d17c9dd30093858dfa8b8562f3e6807f8d299842
Alexandre Böhs <151465216+XandBohs@users.noreply.github.com>
2025-12-24 00:45:44 +0000

    Atualizando a página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
   return (
     <div>
-      <h1>Mudança ao vivo para meu amor</h1>
+      <h1 color="blue">
+        Prazer, me chamo Alexandre e estou aprofundando meus conhecimentos em
+        desenvolvimento web.
+      </h1>
     </div>
   );
 }
