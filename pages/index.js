323e8ce271bd136e97b9f1509e9090ff5dd87680
Alexandre Böhs <151465216+XandBohs@users.noreply.github.com>
2026-02-19 17:08:36 +0000

    voltando o index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h1 color="blue">
+        Prazer, me chamo Alexandre e estou aprofundando meus conhecimentos em
+        desenvolvimento web.
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
